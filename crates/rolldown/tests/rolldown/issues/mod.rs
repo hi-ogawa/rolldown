b5d6e25ee176df6740a18da92a71cdee491b6345
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-08-10 18:34:06 +0900

    Revert "chore: revert asset-import-meta-url plugin"
    This reverts commit 3025b0399d9466a471906cdb96792de7094e7a69.

--- crates/rolldown/tests/rolldown/issues/mod.rs
+++ crates/rolldown/tests/rolldown/issues/mod.rs
@@ -1,2 +1,5 @@
 #[path = "./1733/mod.rs"]
 mod _1733;
+
+#[path = "./repro-new-url-asset/mod.rs"]
+mod _repro_new_url_asset;
--- crates/rolldown_plugin/src/types/hook_transform_ast_args.rs
+++ crates/rolldown_plugin/src/types/hook_transform_ast_args.rs
@@ -1,9 +1,10 @@
-use std::path::PathBuf;
+use std::path::{Path, PathBuf};
 
 use rolldown_ecmascript::EcmaAst;
 
 #[derive(Debug)]
 pub struct HookTransformAstArgs<'a> {
+  pub path: &'a Path,
   pub cwd: &'a PathBuf,
   pub ast: EcmaAst,
 }
